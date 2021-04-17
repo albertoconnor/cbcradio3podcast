532003c0b33cf30b1c6026c6fdb1b420a75181a3
Albert O'Connor <albert@albertoconnor.ca>
2021-04-17 17:56:26 -0400

    Setting up pelican.

---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Me'
+SITENAME = 'CBC Radio 3 Podcast'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Toronto'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "/Users/amjoconn/Development/pelican-themes/Flex"
